b9180f6a69ee0ccb9d89b4ac25dc12daa6b20f04
ColdByDefault <abo.ayash.yazan@gmail.com>
2024-06-02 14:09:46 +0200

    html

--- version_2.0/app/user/routes.py
+++ version_2.0/app/user/routes.py
@@ -2,5 +2,9 @@ from flask import render_template
 from . import user
 
 @user.route('/')
+def login():
+    return render_template('user/login.html')
+
+@user.route('/register')
 def register():
     return render_template('user/register.html')
