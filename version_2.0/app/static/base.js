eb3199de531fe5a6c16456c392a52f8bc009000c
ColdByDefault <abo.ayash.yazan@gmail.com>
2024-06-03 22:34:31 +0200

    styls, projects.html/css/js, themes toggle btn added

--- version_2.0/app/static/base.js
+++ version_2.0/app/static/base.js
@@ -63,3 +63,81 @@ window.addEventListener('resize', () => {
     canvas.height = window.innerHeight;
     initializeStars();  // Reinitialize stars to fit the new canvas size
 });
+
+
+
+
+/* theme btn */
+const themes = [
+    {
+        '--prime-background-color': '#00204a',
+        '--secondary-background-color': '#222',
+        '--color-palette-first': '#005792',
+        '--color-palette-second': '#00bbf0',
+        '--color-palette-third': '#fdb44b', 
+        '--color-palette-fourth': '#41d8bf',
+        '--color-palette-fifth': '#ee5a5a',
+    },
+    {
+        '--prime-background-color': '#000000',
+        '--secondary-background-color': '#14213d',
+        '--color-palette-first': '#22223b',
+        '--color-palette-second': '#4a4e69',
+        '--color-palette-third': '#9a8c98', 
+        '--color-palette-fourth': '#c9ada7',
+        '--color-palette-fifth': '#f2e9e4',
+    }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if there's a theme index saved in local storage
+    let themeIndex = localStorage.getItem('themeIndex');
+    if (themeIndex !== null) {
+        themeIndex = parseInt(themeIndex, 10);
+        applyTheme(themes[themeIndex]);
+    } else {
+        themeIndex = 0;
+        applyTheme(themes[themeIndex]);
+    }
+    localStorage.setItem('themeIndex', themeIndex);
+});
+
+function toggleTheme() {
+    // Get the current theme index from local storage
+    let themeIndex = localStorage.getItem('themeIndex');
+    themeIndex = (parseInt(themeIndex, 10) + 1) % themes.length;
+    
+    // Apply the next theme
+    applyTheme(themes[themeIndex]);
+    
+    // Save the new theme index to local storage
+    localStorage.setItem('themeIndex', themeIndex);
+}
+
+function applyTheme(theme) {
+    for (const [key, value] of Object.entries(theme)) {
+        document.documentElement.style.setProperty(key, value);
+    }
+}
+
+
+
+
+
+
+
+/* '--prime-background-color': '#00204a',
+        '--secondary-background-color': '#222',
+        '--color-palette-first': '#005792',
+        '--color-palette-second': '#00bbf0',
+        '--color-palette-third': '#fdb44b', 
+        '--color-palette-fourth': '#41d8bf',
+        '--color-palette-fifth': '#ee5a5a', */
+
+/* '--prime-background-color': '#000000',
+        '--secondary-background-color': '#14213d',
+        '--color-palette-first': '#22223b',
+        '--color-palette-second': '#4a4e69',
+        '--color-palette-third': '#9a8c98', 
+        '--color-palette-fourth': '#c9ada7',
+        '--color-palette-fifth': '#f2e9e4', */
