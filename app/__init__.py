d482bf15d8b7061beadbbcb9b48ff11514b71f07
ColdByDefault <abo.ayash.yazan@gmail.com>
2024-06-08 20:44:44 +0200

    security vul

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
 
 
 db = SQLAlchemy()
-
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +14,7 @@ def create_app():
     app.config.from_object(Config)
     
     db.init_app(app)
-    
+    csrf.init_app(app)
     
     @app.route('/')
     def home():
--- app/secretKey/secret_key_.py
+++ app/secretKey/secret_key_.py
@@ -3,3 +3,13 @@ import secrets
 # Generate a random URL-safe text string for a secret key
 secret_key = secrets.token_urlsafe(16)
 print("Secret Key:", secret_key)
+
+
+
+
+
+
+# Generate a 32-byte (256-bit) random secret key
+csrf_secret_key = secrets.token_hex(32)
+print(csrf_secret_key)
+
