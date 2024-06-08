6298cfaa4d9772440828dbea42f6811582dbf991
ColdByDefault <abo.ayash.yazan@gmail.com>
2024-06-08 14:49:47 +0200

    books lab added

--- version_2.0/app/library/routes.py
+++ version_2.0/app/library/routes.py
@@ -5,10 +5,21 @@ import os
 @library.route('/')
 def index():
     lab_files_path = os.path.join(os.path.dirname(__file__), 'lab_files')
-    files = os.listdir(lab_files_path)
-    return render_template('library/index.html', files=files)
+    lab_books_path = os.path.join(os.path.dirname(__file__), 'lab_books')
+    
+    files = [(file, 'cheat_sheet') for file in os.listdir(lab_files_path)]
+    books = [(file, 'book') for file in os.listdir(lab_books_path)]
+    
+    items = files + books
+    
+    return render_template('library/index.html', items=items)
 
-@library.route('/download/<filename>')
+@library.route('/download-cheat-sheet/<filename>')
 def download_file(filename):
     lab_files_path = os.path.join(os.path.dirname(__file__), 'lab_files')
     return send_from_directory(lab_files_path, filename, as_attachment=True)
+
+@library.route('/download-book/<filename>')
+def download_book(filename):
+    lab_books_path = os.path.join(os.path.dirname(__file__), 'lab_books')
+    return send_from_directory(lab_books_path, filename, as_attachment=True)
