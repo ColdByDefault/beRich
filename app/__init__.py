3e73e136ab3fdac63716d0bf9adefdae5f610dcb
ColdByDefault <abo.ayash.yazan@gmail.com>
2024-06-08 20:53:50 +0200

    HTTPS, and more security

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,23 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_wtf.csrf import CSRFProtect
+from flask_talisman import Talisman
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
 
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+talisman = Talisman(content_security_policy=None)
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +28,22 @@ def create_app():
     
     db.init_app(app)
     csrf.init_app(app)
+    talisman.init_app(app, force_https=True)
+    limiter.init_app(app)
+    
+    
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App startup')
+    
     
     @app.route('/')
     def home():
--- config.py
+++ config.py
@@ -8,3 +8,6 @@ class Config:
     SECRET_KEY = 'Your Secret Key'
     WTF_CSRF_ENABLED = True
     WTF_CSRF_SECRET_KEY = 'Your CSRF Secret Key'
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
