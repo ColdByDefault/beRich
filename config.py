5c695eb18856574945e2e67027a7e810371f8044
ColdByDefault <abo.ayash.yazan@gmail.com>
2024-06-08 20:32:51 +0200

    refresh venv

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///base_database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'Your Secret Key'
+    WTF_CSRF_ENABLED = True
+    WTF_CSRF_SECRET_KEY = 'Your CSRF Secret Key'
